df140980223359a3d034feb5890d884178ab8f26
Valencia Miyeon Jang <miyeon5230@gmail.com>
2024-07-01 23:14:09 +0900

    second commit

--- main.js
+++ main.js
@@ -10,6 +10,7 @@
 //Reset 버튼을 누르면
 // 랜덤 숫자 생성
 
+/*
 let computerNum = 0;
 let goBtn = document.getElementById("go-btn");
 let userNum = document.getElementById("user-num");
@@ -66,7 +67,7 @@ function play(){
     }
     if(gameOver){
         goBtn.disabled = true;
-        resultArea.textContent = "FAILD";
+        resultArea.textContent = "FAILED";
     }
 }
 
@@ -80,3 +81,81 @@ function reset(){
 }
 
 randomNum();
+*/
+
+//jquery로 위 코드와 같은 내용 구현
+$(document).ready(function(){
+
+    let computerNum = 0;
+    let userNum = $('#user-num').val();
+    let userNumHistory = [];
+    let chances = 5;
+    let gameOver = false;
+
+    //맞춰야 할 숫자 자동 생성    
+    function randomNum(){
+        computerNum = Math.floor(Math.random()*100+1);       
+    }
+    randomNum();    
+    console.log(computerNum);
+
+    //Go버튼을 눌렀을 때
+    $('#go-btn').click(function(){
+        userNum = $('#user-num').val();
+
+        if(userNum < 1 || userNum > 100){
+            $('#result-area').text("1~100사이 숫자를 입력하세요.");
+            return;
+        }
+
+        if(userNumHistory.includes(userNum)){
+            $('#result-area').text("이미 입력한 숫자입니다.");
+            return;
+        }
+
+        chances --;
+        $('#chance-num').text(`남은기회: ${chances}번`);        
+
+        if(userNum < computerNum){
+            $('#result-area').text("UP!!").css('color', 'red').removeClass('blink');;
+        }else if(userNum > computerNum){
+            $('#result-area').text("DOWN!!").css('color', 'green').removeClass('blink');;
+        }else{
+            $('#result-area').text("YOU'RE RIGHT!!!!").addClass("blink");
+            $('#result-img').attr('src', 'https://media3.giphy.com/media/v1.Y2lkPTc5MGI3NjExZzIxZ3Jud2dnYjhmZW12dWZ6NG4zbHF2YmhxeW44enZjNW9zeGtxcyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/U15K44Hl3zvbd1n3oG/giphy.webp').show();
+        }
+
+        userNumHistory.push(userNum);
+
+        if(chances == 0){
+            gameOver = true;
+            $('#result-area').text("DRINK! DRINK!!").addClass('blink');
+            $('#result-img').attr('src', 'https://media0.giphy.com/media/v1.Y2lkPTc5MGI3NjExZGVmNnFtNjZta2hjYzByaGV4ZjM0ZWQ2MXZqbjNrOXM4dGx3aW16dyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/2ePBOQ0LzXv4k/giphy.webp').show();
+            $('#result-img')
+            $('#user-num').val('');
+        }
+
+        if(gameOver){
+            $('#go-btn').prop('disabled', true);
+        }
+    });//go-btn
+
+    //리셋 버튼 눌렀을 때 
+    $('#reset-btn').click(function(){
+        randomNum();
+        console.log(computerNum);
+        gameOver=false;
+        chances = 5;
+        userNumHistory = [];
+        $('#go-btn').prop('disabled', false);
+        $('#chance-num').text(`남은기회: 5번`);
+        $('#result-area').text('');
+        $('#user-num').val('');
+        $('#result-img').attr('src', '').hide();
+    }); //reset-btn
+
+    //input 창에 포커스 갈 때마다 입력된 내용 리셋
+    $('#user-num').focus(function(){
+        $(this).val('');
+    });//input focus
+}); //ready
